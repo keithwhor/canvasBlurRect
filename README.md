feccec5245e2ff72da6766db1fd7d607263308f6
keithwhor <keithwhor@gmail.com>
2015-02-28 17:16:47 -0800

    update README

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # canvasBlurRect
+
 Real-time (30+ FPS) iOS-style box blur + saturation with HTML5 Canvas
+
+canvasBlurRect extends the `CanvasRenderingContext2D` prototype to add a
+  `_blurRect()` method which should perform a box blur + saturation of
+  a rectangle on your canvas and function at > 30FPS on most modern machines.
+
+It does this by shrinking your canvas, bluring the smaller image in JavaScript,
+and using your browser's built-in hardware accelerated scaling to "blow up" your
+blurred image back to full screen.
+
+For best results please use Chrome or Safari (desktop or iOS). Firefox seems
+to not like to perform any "simulated blurring" when it resizes images and will
+give poor results.
+
+Demo available [here](http://keithwhor.com/blur).
+
+# Usage
+
+Include `canvas_blur_rect.js` in your dependencies using:
+
+```html
+<script src="canvas_blur_rect.js"></script>
+```
+
+Once you have a canvas created, you can blur with the following:
+
+HTML:
+
+```html
+<canvas id="my-canvas" width="500" height="500"></canvas>
+```
+
+JavaScript:
+
+```javascript
+var canvas = document.getElementById('my-canvas');
+var context = canvas.getContext('2d');
+
+var x = 0;
+var y = 0;
+var width = 500;
+var height = 500;
+var blurEffect = 1;
+var saturationValue = 2;
+
+context._blurRect(x, y, width, height, blurEffect, saturationValue);
+```
+
+# Reference
+
+### CanvasRenderingContext2d#_blurRect( [Integer] x, [Integer] y, [Integer] w, [Integer] h, [Optional Integer] blur, [Optional Number] sv)
+
+returns self
+
+`x`: The x position corresponding to the upper-left corner of the rectangular area to blur
+
+`y`: The y position corresponding to the upper-left corner of the rectangular area to blur
+
+`w`: The width of the corresponding rectangular area to blur
+
+`h`: The height of the corresponding rectangular area to blur
+
+`blur`: Optional. The value of the blur effect. Corresponds to a blur radius multiplied by 32
+ (for performance reasons.) Default is 1.
+
+ `sv`: Optional. Saturation value. Provide a value between 0 (completely desaturated, black + white) and
+ 2, with 1 being normal saturation. Default is 1.
+
+ # Want to Help?
+
+ The code was whipped together quickly, and I've repeated myself over a number
+ of methods for performance reasons. If you can milk better performance out of
+ this library, PRs are welcome! Thanks!
+
+ # Acknowledgements
+
+ This is a small library, made for fun. Wanted to see if I could pull off
+ fast box blurring in canvas. Enjoy! :)
+
+ Follow me on Twitter, [@keithwhor](http://twitter.com/keithwhor)
+
+ Or check out my personal website at [keithwhor.com](http://keithwhor.com)
---
+++ canvas_blur_rect.js
@@ -0,0 +1,335 @@
+!function(window) {
+
+  'use strict';
+
+  var blurCanvas = document.createElement('canvas');
+  blurCanvas.width = screen.width;
+  blurCanvas.height = screen.height;
+  var blurCtx = blurCanvas.getContext('2d');
+
+  function saturate(src, w, h, sv) {
+
+    var len = w * h;
+    var pos, i, j, offset;
+
+    var lumR = (1 - sv) * 0.3086;
+    var lumG = (1 - sv) * 0.6094;
+    var lumB = (1 - sv) * 0.0820;
+
+    var r, g, b;
+
+    var shiftW = w << 2;
+
+    for(j = 0; j < h; j++) {
+
+      offset = j * shiftW;
+
+      for(i = 0; i < w; i++) {
+
+        pos = offset + (i << 2);
+
+        r = src[pos];
+        g = src[pos + 1];
+        b = src[pos + 2];
+
+        src[pos] = ((lumR + sv) * r) +
+          (lumG * g) +
+          (lumB * b);
+
+        src[pos + 1] = (lumR * r) +
+          ((lumG + sv) * g) +
+          (lumB * b);
+
+        src[pos + 2] = (lumR * r) +
+          (lumG * g) +
+          ((lumB + sv) * b);
+
+      }
+
+    }
+
+  };
+
+  function boxBlur(src, w, h, r, sv) {
+    var tmp = new Uint8Array(w * h * 4);
+    blurRight(src, tmp, w, h, r);
+    blurDown(tmp, src, w, h, r);
+    blurLeft(src, tmp, w, h, r);
+    blurUp(tmp, src, w, h, r);
+    (sv !== undefined && sv !== 1) && saturate(src, w, h, sv);
+  };
+
+  function blurRight(src, dest, w, h, r) {
+
+    var i, j, offset, pos, posR;
+
+    var shiftR = r << 2;
+    var shiftW = w << 2;
+
+    var weightR, weightG, weightB, weightA;
+
+    for(j = 0; j < h; j++) {
+
+      weightR = 0;
+      weightG = 0;
+      weightB = 0;
+      weightA = 0;
+
+      offset = j * shiftW;
+
+      for(i = 0; i < r; i++) {
+
+        pos = offset + (i << 2);
+
+        weightR += src[pos];
+        weightG += src[pos + 1];
+        weightB += src[pos + 2];
+        weightA += src[pos + 3];
+
+        dest[pos] = (weightR / (i + 1)) | 0;
+        dest[pos + 1] = (weightG / (i + 1)) | 0;
+        dest[pos + 2] = (weightB / (i + 1)) | 0;
+        dest[pos + 3] = (weightA / (i + 1)) | 0;
+
+      }
+
+      for(; i < w; i++) {
+
+        pos = offset + (i << 2);
+        posR = pos - shiftR;
+
+        dest[pos] = (weightR / r) | 0;
+        dest[pos + 1] = (weightG / r) | 0;
+        dest[pos + 2] = (weightB / r) | 0;
+        dest[pos + 3] = (weightA / r) | 0;
+
+        weightR += src[pos] - src[posR];
+        weightG += src[pos + 1] - src[posR + 1];
+        weightB += src[pos + 2] - src[posR + 2];
+        weightA += src[pos + 3] - src[posR + 3];
+
+      }
+
+    }
+
+  };
+
+  function blurLeft(src, dest, w, h, r) {
+
+    var i, j, offset, pos, posR;
+
+    var shiftR = r << 2;
+    var shiftW = w << 2;
+
+    var weightR, weightG, weightB, weightA;
+
+    for(j = 0; j < h; j++) {
+
+      weightR = 0;
+      weightG = 0;
+      weightB = 0;
+      weightA = 0;
+
+      offset = j * shiftW;
+
+      for(i = w - 1; i >= w - r; i--) {
+
+        pos = offset + (i << 2);
+
+        weightR += src[pos];
+        weightG += src[pos + 1];
+        weightB += src[pos + 2];
+        weightA += src[pos + 3];
+
+        dest[pos] = (weightR / (w - i)) | 0;
+        dest[pos + 1] = (weightG / (w - i)) | 0;
+        dest[pos + 2] = (weightB / (w - i)) | 0;
+        dest[pos + 3] = (weightA / (w - i)) | 0;
+
+      }
+
+      for(; i >= 0; i--) {
+
+        pos = offset + (i << 2);
+        posR = pos + shiftR;
+
+        dest[pos] = (weightR / r) | 0;
+        dest[pos + 1] = (weightG / r) | 0;
+        dest[pos + 2] = (weightB / r) | 0;
+        dest[pos + 3] = (weightA / r) | 0;
+
+        weightR += src[pos] - src[posR];
+        weightG += src[pos + 1] - src[posR + 1];
+        weightB += src[pos + 2] - src[posR + 2];
+        weightA += src[pos + 3] - src[posR + 3];
+
+      }
+
+    }
+
+  };
+
+  function blurDown(src, dest, w, h, r) {
+
+    var i, j, offset, pos, posR;
+
+    var shiftR = r << 2;
+    var shiftW = w << 2;
+
+    var offsetR = shiftW * r;
+
+    var weightR, weightG, weightB, weightA;
+
+    for(i = 0; i < w; i++) {
+
+      weightR = 0;
+      weightG = 0;
+      weightB = 0;
+      weightA = 0;
+
+      offset = i << 2;
+
+      for(j = 0; j < r; j++) {
+
+        pos = offset + (j * shiftW);
+
+        weightR += src[pos];
+        weightG += src[pos + 1];
+        weightB += src[pos + 2];
+        weightA += src[pos + 3];
+
+        dest[pos] = (weightR / (j + 1)) | 0;
+        dest[pos + 1] = (weightG / (j + 1)) | 0;
+        dest[pos + 2] = (weightB / (j + 1)) | 0;
+        dest[pos + 3] = (weightA / (j + 1)) | 0;
+
+      }
+
+      for(; j < h; j++) {
+
+        pos = offset + (j * shiftW);
+        posR = pos - offsetR;
+
+        dest[pos] = (weightR / r) | 0;
+        dest[pos + 1] = (weightG / r) | 0;
+        dest[pos + 2] = (weightB / r) | 0;
+        dest[pos + 3] = (weightA / r) | 0;
+
+        weightR += src[pos] - src[posR];
+        weightG += src[pos + 1] - src[posR + 1];
+        weightB += src[pos + 2] - src[posR + 2];
+        weightA += src[pos + 3] - src[posR + 3];
+
+      }
+
+    }
+
+  };
+
+  function blurUp(src, dest, w, h, r) {
+
+    var i, j, offset, pos, posR;
+
+    var shiftR = r << 2;
+    var shiftW = w << 2;
+
+    var offsetR = shiftW * r;
+
+    var weightR, weightG, weightB, weightA;
+
+    for(i = 0; i < w; i++) {
+
+      weightR = 0;
+      weightG = 0;
+      weightB = 0;
+      weightA = 0;
+
+      offset = i << 2;
+
+      for(j = h - 1; j >= h - r; j--) {
+
+        pos = offset + (j * shiftW);
+
+        weightR += src[pos];
+        weightG += src[pos + 1];
+        weightB += src[pos + 2];
+        weightA += src[pos + 3];
+
+        dest[pos] = (weightR / (h - j)) | 0;
+        dest[pos + 1] = (weightG / (h - j)) | 0;
+        dest[pos + 2] = (weightB / (h - j)) | 0;
+        dest[pos + 3] = (weightA / (h - j)) | 0;
+
+      }
+
+      for(; j >= 0; j--) {
+
+        pos = offset + (j * shiftW);
+        posR = pos + offsetR;
+
+        dest[pos] = (weightR / r) | 0;
+        dest[pos + 1] = (weightG / r) | 0;
+        dest[pos + 2] = (weightB / r) | 0;
+        dest[pos + 3] = (weightA / r) | 0;
+
+        weightR += src[pos] - src[posR];
+        weightG += src[pos + 1] - src[posR + 1];
+        weightB += src[pos + 2] - src[posR + 2];
+        weightA += src[pos + 3] - src[posR + 3];
+
+      }
+
+    }
+
+  };
+
+  function blurRect(x, y, w, h, r, sv) {
+
+    var ctx = this;
+    var canvas = ctx.canvas;
+
+    var srcW = w | 0;
+    var srcH = h | 0;
+
+    var srcX = x | 0;
+    var srcY = y | 0;
+
+    r = (r | 0) * 32;
+    r = Math.min(Math.max(r, 32), 256);
+
+    var resizeFactor = Math.max(0, ((Math.log(r) / Math.log(2)) - 3) | 0);
+    var radius = r >>> resizeFactor;
+
+    var resizeWidth = canvas.width >>> resizeFactor;
+    var resizeHeight = canvas.height >>> resizeFactor;
+
+    blurCtx.drawImage(canvas, 0, 0, resizeWidth, resizeHeight);
+    var imageData = blurCtx.getImageData(0, 0, resizeWidth, resizeHeight);
+
+    boxBlur(imageData.data, resizeWidth, resizeHeight, radius, sv);
+
+    blurCtx.putImageData(imageData, 0, 0);
+
+    blurCtx.drawImage(
+      blurCanvas,
+      0, 0,
+      resizeWidth, resizeHeight,
+      0, 0,
+      canvas.width, canvas.height
+    );
+
+    ctx.drawImage(
+      blurCanvas,
+      srcX, srcY,
+      srcW, srcH,
+      srcX, srcY,
+      srcW, srcH
+    );
+
+    returns ctx;
+
+  };
+
+  window.CanvasRenderingContext2D.prototype._blurRect = blurRect;
+
+}(window);
